73467e7b89eb5375d8f1b7515ffa540a7c64da85
Ranvir Sahota <rsahota666@gmail.com>
2020-02-24 15:04:59 +0000

    created registration ui

---
+++ app/src/main/java/com/example/medical/UI/Registration.java
@@ -0,0 +1,17 @@
+//Name:
+package com.example.medical.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.medical.R;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
