f9596fc1a0efdc5054a19afc3cd88b3824a56c1c
Ranvir Sahota <rsahota666@gmail.com>
2020-02-24 15:01:30 +0000

    created list of events

---
+++ app/src/main/java/com/example/medical/UI/ListOfEvents.java
@@ -0,0 +1,18 @@
+//Name:
+
+package com.example.medical.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.medical.R;
+
+public class ListOfEvents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_events);
+    }
+}
