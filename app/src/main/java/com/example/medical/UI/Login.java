2705550221d664333f9d720b41e21e03a9b1d6f5
AndreiB <up845111@myport.ac.uk>
2020-03-13 22:07:11 +0000

    added functionality to home button

--- app/src/main/java/com/example/medical/UI/Login.java
+++ app/src/main/java/com/example/medical/UI/Login.java
@@ -4,7 +4,9 @@ package com.example.medical.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.medical.R;
 
@@ -14,5 +16,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void homeButton (View view){
+
+        Intent homeButt = new Intent( Login.this, Calender.class);
+        startActivity(homeButt);
     }
 }
