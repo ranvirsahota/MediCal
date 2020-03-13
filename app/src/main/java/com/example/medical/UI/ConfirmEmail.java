07ab49245dbc61cc49567f5324d72233a9c08c70
AndreiB <up845111@myport.ac.uk>
2020-03-13 23:03:23 +0000

    added a confirmEmail activity

---
+++ app/src/main/java/com/example/medical/UI/ConfirmEmail.java
@@ -0,0 +1,23 @@
+package com.example.medical.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.medical.R;
+
+public class ConfirmEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.confirm_email);
+    }
+
+    public void homeButton (View view){
+
+        Intent homeButt = new Intent( ConfirmEmail.this, Calender.class);
+        startActivity(homeButt);
+    }
+}
