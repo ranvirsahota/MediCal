1d7e59acedf6dec4d25b6ab1c39be71dd961f07d
AndreiB <up845111@myport.ac.uk>
2020-03-15 18:50:02 +0000

    created home page with buttons with no functionality

---
+++ app/src/main/java/com/example/medical/UI/homePage.java
@@ -0,0 +1,15 @@
+package com.example.medical.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.medical.R;
+
+public class homePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
