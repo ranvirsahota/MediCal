5ca855d80b5befe0b3898fd282ffd9682485c533
AndreiB <up845111@myport.ac.uk>
2020-03-19 11:09:35 +0000

    added functionality to back button

--- app/src/main/java/com/example/medical/UI/Registration.java
+++ app/src/main/java/com/example/medical/UI/Registration.java
@@ -3,7 +3,9 @@ package com.example.medical.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.medical.R;
 
@@ -13,5 +15,11 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public void backButton (View view){
+
+        Intent backButt = new Intent( Registration.this, Login.class);
+        startActivity(backButt);
     }
 }
