46920b451aa6cefb5bad98180564ce1dc7975ba0
AndreiB <up845111@myport.ac.uk>
2020-03-19 10:13:39 +0000

    added functionality to the calendar and sign in buttons

--- app/src/main/java/com/example/medical/UI/homePage.java
+++ app/src/main/java/com/example/medical/UI/homePage.java
@@ -2,7 +2,10 @@ package com.example.medical.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import com.example.medical.R;
 
 public class homePage extends AppCompatActivity {
@@ -11,5 +14,17 @@ public class homePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+
+    public void calendarBtn (View view){
+
+        Intent calBtn = new Intent( homePage.this, Calender.class);
+        startActivity(calBtn);
+    }
+
+    public void signIn (View view){
+
+        Intent signIn = new Intent( homePage.this, Login.class);
+        startActivity(signIn);
     }
 }
