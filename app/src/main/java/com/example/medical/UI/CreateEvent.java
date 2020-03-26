20a0bbaf8772a00339f7387fa748c0ab4e0ffae0
AndreiB <up845111@myport.ac.uk>
2020-03-26 15:42:18 +0000

    added new strings

---
+++ app/src/main/java/com/example/medical/UI/CreateEvent.java
@@ -0,0 +1,13 @@
+package com.example.medical.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+    }
+}
