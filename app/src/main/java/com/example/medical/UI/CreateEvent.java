6d507da0c4bb5553c2a84a19e9b534adfcca44e8
WAQAR77394 <55918958+WAQAR77394@users.noreply.github.com>
2020-03-26 18:33:23 +0000

    Created the create event page
     text boxes and buttons with functionality

--- app/src/main/java/com/example/medical/UI/CreateEvent.java
+++ app/src/main/java/com/example/medical/UI/CreateEvent.java
@@ -1,7 +1,11 @@
 package com.example.medical.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.medical.R;
 
 public class CreateEvent extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class CreateEvent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+    }
+
+    public void homeButton (View view){
+
+        Intent homeButt = new Intent( CreateEvent.this, homePage.class);
+        startActivity(homeButt);
     }
 }
