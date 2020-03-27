9bc9d3a0643941b53d4a7894ac531d362bbecb48
AndreiB <up845111@myport.ac.uk>
2020-03-27 10:39:03 +0000

    created accesibility activity

---
+++ app/src/main/java/com/example/medical/UI/Accessibility.java
@@ -0,0 +1,14 @@
+package com.example.medical.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Accessibility extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accessibility);
+    }
+}
